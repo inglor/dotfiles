fb8146bd05d18fdc3d379c67925d5c0e7416356f
Leonidas Spyropoulos <artafinde@gmail.com>
2016-12-18 10:25:51 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 install_dot_file(){
-  echo "Installing dot file $1 in ${HOME} directory"
-  ln -s $1 "${HOME}/.$1"
+  echo "Installing dot file '$2/$1' in '${HOME}/.$1' directory"
+  ln -s "$2/$1" "${HOME}/.$1"
 }
 
 SCRIPT=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT")
 cd "$DIR"
 
-install_dot_file vimrc
+install_dot_file vimrc $DIR
